4f131f141590ea6ad88eeb4a027b0eaa93f40174
Yeferson Nova <yeferson_nova@outlook.com>
2025-05-14 23:51:29 -0500

    inicio del proyecto bump

---
+++ src/main/java/com/ynova/bump/BumpApplication.java
@@ -0,0 +1,13 @@
+package com.ynova.bump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BumpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynova/bump/BumpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynova.bump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
